ddca8fa1fbf1de79cc93a7121c0b7c12b667a0eb
Savov,Kristian K.K <k.savov@student.fontys.nl>
2021-05-29 21:17:38 +0300

    Register form validation user creation working

--- clonnit/classes/Userdata.class.php
+++ clonnit/classes/Userdata.class.php
@@ -1,68 +1,92 @@
 <?php
-include_once ('PDO.class.php');
+include_once 'PDO.class.php';
 
-class Userdata extends Connection{
-
-
-public function RegisterAUser($newusername,$newemail,$newpassword){
-    $sql = 'INSERT INTO user(username,email,password) VALUES(:username,:email,:password)';
+class Userdata extends Connection
+{
+  public function RegisterAUser($newusername, $newemail, $newpassword)
+  {
+    $sql =
+      'INSERT INTO user(username,email,password) VALUES(:username,:email,:password)';
     $stmt = $this->Connect()->prepare($sql);
-    $stmt->execute(['username' => $newusername, 'email' => $newemail, 'password' => $newpassword]);
-}
+    $stmt->execute([
+      'username' => $newusername,
+      'email' => $newemail,
+      'password' => $newpassword,
+    ]);
+  }
 
-
-public function UpdateEmail($newemail,$newid){
+  public function UpdateEmail($newemail, $newid)
+  {
     $sql = 'UPDATE user SET email = :email WHERE id = :id';
     $stmt = $this->Connect()->prepare($sql);
-    $stmt->execute(['email' => $newemail,'id' => $newid]);
-}
+    $stmt->execute(['email' => $newemail, 'id' => $newid]);
+  }
 
-
-public function UpdatePassword($newpassowrd,$providedid){
+  public function UpdatePassword($newpassowrd, $providedid)
+  {
     $sql = 'UPDATE user SET password = :password WHERE id = :id';
     $stmt = $this->Connect()->prepare($sql);
     $stmt->execute(['password' => $newpassowrd, 'id' => $providedid]);
-}
+  }
 
-public function GetAUser($email){
+  public function GetAUser($email)
+  {
     $sql = 'SELECT * FROM user WHERE email =?';
     $stmt = $this->Connect()->prepare($sql);
     $stmt->execute([$email]);
     $user = $stmt->fetch();
     $fid = $user->id;
-    $fusername= $user->username;
+    $fusername = $user->username;
     $fpassword = $user->password;
     $femail = $user->email;
-    $founduser = new User($fid,$fusername,$fpassword,$femail);
+    $founduser = new User($fid, $fusername, $fpassword, $femail);
     return $founduser;
-}
+  }
 
-public function GetAUserByUsername($username){
+  public function GetAUserByUsername($username)
+  {
     $sql = 'SELECT * FROM user WHERE username =?';
     $stmt = $this->Connect()->prepare($sql);
     $stmt->execute([$username]);
     $user = $stmt->fetch();
     $fid = $user->id;
-    $fusername= $user->username;
+    $fusername = $user->username;
     $fpassword = $user->password;
     $femail = $user->email;
-    $founduser = new User($fid,$fusername,$fpassword,$femail);
+    $founduser = new User($fid, $fusername, $fpassword, $femail);
     return $founduser;
-}
+  }
 
-public function GetAUserByID($id){
+  public function GetAUserByID($id)
+  {
     $sql = 'SELECT * FROM user WHERE id =?';
     $stmt = $this->Connect()->prepare($sql);
     $stmt->execute([$id]);
     $user = $stmt->fetch();
     $fid = $user->id;
-    $fusername= $user->username;
+    $fusername = $user->username;
     $fpassword = $user->password;
     $femail = $user->email;
-    $founduser = new User($fid,$fusername,$fpassword,$femail);
+    $founduser = new User($fid, $fusername, $fpassword, $femail);
     return $founduser;
-}
+  }
 
+  public function GetUsernameCount($username)
+  {
+    $sql = "SELECT * from user where username='{$username}'";
+    $stmt = $this->Connect()->prepare($sql);
+    $stmt->execute();
+    $usernameCount = $stmt->rowCount();
+    return $usernameCount;
+  }
 
+  public function GetEmailCount($email)
+  {
+    $sql = "SELECT * from user where email='{$email}'";
+    $stmt = $this->Connect()->prepare($sql);
+    $stmt->execute();
+    $emailCount = $stmt->rowCount();
+    return $emailCount;
+  }
 }
 ?>
--- clonnit/reg.php
+++ clonnit/reg.php
@@ -1,19 +1,13 @@
 <?php
 include_once './includes/autoload.inc.php';
 
-$username = $_POST['username'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$confirmPassword = $_POST['confirmPassword'];
+$username = htmlspecialchars($_POST['username']);
+$email = htmlspecialchars($_POST['email']);
+$password = htmlspecialchars($_POST['password']);
+$confirmPassword = htmlspecialchars($_POST['confirmPassword']);
 
 $ok = false;
 $canRegister = false;
-
-// $register->RegisterAUser(
-//   $username,
-//   $email,
-//   password_hash($password, PASSWORD_DEFAULT)
-// );
 
 if (!$username && !$email && !$password && !$confirmPassword) {
   echo 'All fields required!';
@@ -52,8 +46,6 @@ if (!$username && !$email && !$password && !$confirmPassword) {
             echo 'Passwords should match!';
             $ok = false;
           } else {
-            //success
-            //echo '<span id="success">Successfull registration!</span>';
             $ok = true;
           }
         }
@@ -62,85 +54,37 @@ if (!$username && !$email && !$password && !$confirmPassword) {
   }
 }
 
+//check for dublicates in db
 if ($ok == true) {
   $register = new Userdata();
   $usernameOk = false;
   $emailOk = false;
-  $usernameExist =
-    $register->GetAUserByUsername($username)->GetUsername() === null
-      ? false
-      : true;
-  $emailExist = $register->GetAUser($email)->GetEmail() === null ? false : true;
+  $usernameExist = $register->GetUsernameCount($username) == 0 ? false : true;
+  $emailExist = $register->GetEmailCount($email) == 0 ? false : true;
 
-  // if ($register->GetAUserByUsername($username) != null) {
-  //   echo 'Username is already taken!';
-  //   $usernameOk = false;
-  //   $canRegister = false;
-  // } else {
-  //   $usernameOk = true;
-  // }
-
-  // if ($register->GetAUser($email) != null) {
-  //   echo 'Email is already taken!';
-  //   $canRegister = false;
-  // } else {
-  //   $emailOk = true;
-  // }
-
-  // if (
-  //   $register->GetAUserByUsername($username) === null &&
-  //   $register->GetAUser($email) === null
-  // ) {
-  //   $canRegister = true;
-  // }
-
-  // if ($usernameExist && $emailExist) {
-  //   $usernameOk = false;
-  //   $emailOk = false;
-  // } elseif (!$usernameExist && $emailExist) {
-  //   $usernameOk = true;
-  //   $emailOk = false;
-  // } elseif ($usernameExist && !$emailExist) {
-  //   $usernameOk = false;
-  //   $emailOk = true;
-  // } elseif (!$usernameExist && !$emailExist) {
-  //   $usernameOk = true;
-  //   $emailOk = true;
-  // }
-
-  // if ($usernameExist) {
-  //   $usernameOk = false;
-  // } else {
-  //   $usernameOk = true;
-  // }
-  // if ($emailExist) {
-  //   $emailOk = false;
-  // } else {
-  //   $emailOk = true;
-  // }
+  if ($usernameExist && $emailExist) {
+    echo 'User with this username/email already exists!';
+    $canRegister = false;
+  } elseif ($usernameExist && !$emailExist) {
+    echo 'Username is already taken!';
+    $canRegister = false;
+  } elseif (!$usernameExist && $emailExist) {
+    echo 'Email is already in use!';
+    $canRegister = false;
+  } else {
+    $canRegister = true;
+  }
 }
-echo $usernameExist;
-echo $emailExist;
 
-// if ($usernameExist && $emailExist) {
-//   echo 'Username and email are already in use!';
-//   $canRegister = false;
-// } else {
-//   if ($usernameExist == true) {
-//   }
-// }
-
-// } elseif (
-//   $GotUserName->GetUsername() == null &&
-//   $GotUserEmail->GetEmail() != null
-// ) {
-//   echo 'Email is already taken!';
-//   $canRegister = false;
-// } elseif (
-//   $GotUserName->GetUsername() == null &&
-//   $GotUserEmail->GetEmail() == null
-// ) {
-//   $canRegister = true;
-// }
-
+//create user
+if ($canRegister == true) {
+  $register = new Userdata();
+  $register->RegisterAUser(
+    $username,
+    $email,
+    password_hash($password, PASSWORD_DEFAULT)
+  );
+  //success
+  echo '<span id="success">Successfull registration!</span>';
+}
 ?>
