c43d0829f6be0f10019b1808ac5b329717351f1c
Savov,Kristian K.K <k.savov@student.fontys.nl>
2021-05-30 19:15:26 +0300

    fixed some css

--- clonnit/index.php
+++ clonnit/index.php
@@ -1,87 +1,105 @@
 <!DOCTYPE html>
 
-<?php include("./templates/header.php");
-include_once('./includes/autoload.inc.php');
-
+<?php
+include './templates/header.php';
+include_once './includes/autoload.inc.php';
 ?>
 
-<section class="credentials-container">
-<?php 
-if(isset($_SESSION["username"])){
-    echo "<p>Welcome " . $_SESSION["username"] ." <p>";
-}
-?>
+<section class="credentials-container" style="display:grid">
+    <?php if (isset($_SESSION['username'])) {
+      echo '<p id="welcome">Welcome ' . $_SESSION['username'] . ' <p>';
+    } ?>
 
-<main>
-            
+    <main>
 
-                <?php 
-                $userdata = new Userdata();
-                $posts = new Contentdata();
 
-                if (isset($_GET['search'])){
-                    $serach = $_GET['search'];
-                    $array = $posts -> SerachPosts($serach);  
-                    
-                    if(count($array) === 0){
-                        echo "No posts found that match the criteria";
-                    }
-                    else {
-                        foreach($array as $value){
-                            if(isset($_SESSION['id']))
-                        {
-                            $user = $userdata->GetAUserByID($_SESSION['id']);
-                            if($_SESSION['id'] === $value->Getauthor_id() or $user->GetAdminStatus() == 1){
-                                $var_id = $value->GetID();
-                                echo '<a href="post.php?var_id=' . $var_id . '">  <div class="left-title">  <h2> ' . $value->Gettitle() . ' </h2> </a> '; 
-                                echo  '<a href="./scripts/deletePost.php?var_id=' . $var_id . '">  <p> Delete </p></a> </div> <br>'  ;
-                            }
-                            else {
-                                $var_id = $value->GetID();
-                                echo '<a href="post.php?var_id=' . $var_id . '">  <div class="left-title">  <h2> ' . $value->Gettitle() . ' </h2> </a> '; 
-                            }
-                        }
-                        else {
-                            $var_id = $value->GetID();
-                            echo '<a href="post.php?var_id=' . $var_id . '">  <div class="left-title">  <h2> ' . $value->Gettitle() . ' </h2> </a> </div> <br>';    
-                        }
-                        }
+        <?php
+        $userdata = new Userdata();
+        $posts = new Contentdata();
 
-                    }
-                  
+        if (isset($_GET['search'])) {
+          $serach = $_GET['search'];
+          $array = $posts->SerachPosts($serach);
 
+          if (count($array) === 0) {
+            echo 'No posts found that match the criteria';
+          } else {
+            foreach ($array as $value) {
+              if (isset($_SESSION['id'])) {
+                $user = $userdata->GetAUserByID($_SESSION['id']);
+                if (
+                  $_SESSION['id'] === $value->Getauthor_id() or
+                  $user->GetAdminStatus() == 1
+                ) {
+                  $var_id = $value->GetID();
+                  echo '<a href="post.php?var_id=' .
+                    $var_id .
+                    '">  <div class="left-title">  <h2> ' .
+                    $value->Gettitle() .
+                    ' </h2> </a> ';
+                  echo '<a href="./scripts/deletePost.php?var_id=' .
+                    $var_id .
+                    '">  <p> Delete </p></a> </div> <br>';
+                } else {
+                  $var_id = $value->GetID();
+                  echo '<a href="post.php?var_id=' .
+                    $var_id .
+                    '">  <div class="left-title">  <h2> ' .
+                    $value->Gettitle() .
+                    ' </h2> </a> ';
                 }
-                else {
-                    $array = $posts -> GetAllPosts();   
-                    foreach($array as $value){
-                        if(isset($_SESSION['id']))
-                        {
-                            $user = $userdata->GetAUserByID($_SESSION['id']);
-                            if($_SESSION['id'] === $value->Getauthor_id() or $user->GetAdminStatus() == 1){
-                                $var_id = $value->GetID();
-                                echo '<a href="post.php?var_id=' . $var_id . '">  <div class="left-title">  <h2> ' . $value->Gettitle() . ' </h2> </a> '; 
-                                echo  '<a href="./scripts/deletePost.php?var_id=' . $var_id . '">  <p> Delete </p></a> </div> <br>'  ;
-                            }
-                            else {
-                                $var_id = $value->GetID();
-                                echo '<a href="post.php?var_id=' . $var_id . '">  <div class="left-title">  <h2> ' . $value->Gettitle() . ' </h2> </a> '; 
-                            }
-                        }
-                        else {
-                            $var_id = $value->GetID();
-                            echo '<a href="post.php?var_id=' . $var_id . '">  <div class="left-title">  <h2> ' . $value->Gettitle() . ' </h2> </a> </div> <br>';    
-                        }
-                       
-                    }
-                }
-               
-                
-                ?>
+              } else {
+                $var_id = $value->GetID();
+                echo '<a href="post.php?var_id=' .
+                  $var_id .
+                  '">  <div class="left-title">  <h2> ' .
+                  $value->Gettitle() .
+                  ' </h2> </a> </div> <br>';
+              }
+            }
+          }
+        } else {
+          $array = $posts->GetAllPosts();
+          foreach ($array as $value) {
+            if (isset($_SESSION['id'])) {
+              $user = $userdata->GetAUserByID($_SESSION['id']);
+              if (
+                $_SESSION['id'] === $value->Getauthor_id() or
+                $user->GetAdminStatus() == 1
+              ) {
+                $var_id = $value->GetID();
+                echo '<a href="post.php?var_id=' .
+                  $var_id .
+                  '">  <div class="left-title">  <h2> ' .
+                  $value->Gettitle() .
+                  ' </h2> </a> ';
+                echo '<a href="./scripts/deletePost.php?var_id=' .
+                  $var_id .
+                  '">  <p> Delete </p></a> </div> <br>';
+              } else {
+                $var_id = $value->GetID();
+                echo '<a href="post.php?var_id=' .
+                  $var_id .
+                  '">  <div class="left-title">  <h2> ' .
+                  $value->Gettitle() .
+                  ' </h2> </a> ';
+              }
+            } else {
+              $var_id = $value->GetID();
+              echo '<a href="post.php?var_id=' .
+                $var_id .
+                '">  <div class="left-title">  <h2> ' .
+                $value->Gettitle() .
+                ' </h2> </a> </div> <br>';
+            }
+          }
+        }
+        ?>
 
-       
+
     </main>
 
 </section>
-<?php include("./templates/footer.php");?>
+<?php include './templates/footer.php'; ?>
 
 </html>
--- clonnit/post.php
+++ clonnit/post.php
@@ -1,111 +1,103 @@
 <!DOCTYPE html>
 
 <?php
-  include("./templates/header.php");
-  include_once('./includes/autoload.inc.php');
+include './templates/header.php';
+include_once './includes/autoload.inc.php';
 
-$errors = array('comment'=>'');
+$errors = ['comment' => ''];
 $comment = '';
-    if(isset($_POST['creation']))
-    {
-        //check content
-        if(empty($_POST['comment']))
-        {
-            $errors['comment'] = 'Please enter comment<br />';
-        }
-        else
-        {
-            $comment = $_POST['comment'];
-        }
-        
-        if(array_filter($errors))
-        {
-            echo '';
-        }
-        else
-        {
-            $posts = new Contentdata();
-            $comment = $_POST["comment"];
-            $var_value = $_GET['var_id'];
-            $id = $_SESSION["id"];
+if (isset($_POST['creation'])) {
+  //check content
+  if (empty($_POST['comment'])) {
+    $errors['comment'] = 'Please enter comment<br />';
+  } else {
+    $comment = $_POST['comment'];
+  }
 
-            $posts->PostComment($id,$var_value,$comment);
-            echo "Nice";
+  if (array_filter($errors)) {
+    echo '';
+  } else {
+    $posts = new Contentdata();
+    $comment = $_POST['comment'];
+    $var_value = $_GET['var_id'];
+    $id = $_SESSION['id'];
 
-
-
-        }
-    }
-
+    $posts->PostComment($id, $var_value, $comment);
+    echo 'Nice';
+  }
+}
 ?>
 
 <section class="credentials-container">
 
 
-<main>
-            
+    <main>
 
-                <?php 
 
-                     $var_value = $_GET['var_id'];
-                     $posts = new Contentdata();
-                     $users = new Userdata();
-                     $passedpost = $posts->GetAPostByID($var_value);
-                     $id = $passedpost->Getauthor_id();
-                     $user = $users->GetAUserByID($id);
+        <?php
+        $var_value = $_GET['var_id'];
+        $posts = new Contentdata();
+        $users = new Userdata();
+        $passedpost = $posts->GetAPostByID($var_value);
+        $id = $passedpost->Getauthor_id();
+        $user = $users->GetAUserByID($id);
 
-                     echo '<div class="left-title">  <h3> ' . $passedpost->Gettitle() . ' </h3> 
-                     <p> Posted by:' . $user->GetUsername() .'</p>
+        echo '<div class="left-title">  <h3> ' .
+          $passedpost->Gettitle() .
+          ' </h3> 
+                     <p style="font-size:12px"> Posted by: ' .
+          $user->GetUsername() .
+          '</p>
                      </div> <br>';
-                      echo ' <div class="left-title">  <p> ' . $passedpost->Getcontent() . '</p> </div> <br>';
-                      ?>
-                    
-                    
-                    <section class="post-container">
+        echo ' <div class="left-title">  <p> ' .
+          $passedpost->Getcontent() .
+          '</p> </div> <br>';
+        ?>
 
-                    <?php
-                    if(isset($_SESSION["id"])){
-                        echo ' <form action="post.php?var_id=' .$var_value . '" method="POST">
-                        <textarea name="comment" placeholder="Comment...">' . htmlspecialchars($comment) . '</textarea><br>
-                        <div class="error-msg-credentials">' .$errors["comment"]. '</div>
+
+        <section class="post-container">
+
+            <?php if (isset($_SESSION['id'])) {
+              echo ' <form action="post.php?var_id=' .
+                $var_value .
+                '" method="POST">
+                        <textarea name="comment" placeholder="Comment...">' .
+                htmlspecialchars($comment) .
+                '</textarea><br>
+                        <div class="error-msg-credentials">' .
+                $errors['comment'] .
+                '</div>
                          <input type="submit" name="creation" value="Create"><br>
-                         </form> ' ;
-                    }
-                   
-                    
-                    ?>
-                   
-                    
-                       </section>
-
-                   
-                    
-                    
-                   
-                  
-
-                     <?php
-                     
-                     $array = $posts->GetAllComments($var_value);
-
-                     foreach($array as $value){
-                        $author_id = $value->GetAuthorID();
-                        $user = $users->GetAUserByID($author_id);
-                         echo  $user->GetUsername() . ': ' . $value->GetComment() . '<br>';
+                         </form> ';
+            } ?>
 
 
-                     }
-                     
-                     ?>
+        </section>
 
-       
+
+
+
+
+
+
+        <?php
+        $array = $posts->GetAllComments($var_value);
+
+        foreach ($array as $value) {
+          $author_id = $value->GetAuthorID();
+          $user = $users->GetAUserByID($author_id);
+          echo $user->GetUsername() . ': ' . $value->GetComment() . '<br>';
+        }
+        ?>
+
+
     </main>
 
-   
-    
-   
+
+
+
 
 </section>
-<?php include("./templates/footer.php");?>
+<?php include './templates/footer.php'; ?>
 
 </html>
